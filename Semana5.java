dca373b7a9eb019acf6043383504c4ea6da8cd62
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-09-02 20:01:58 +0000

    2 exercicio

--- Semana5.java
+++ Semana5.java
@@ -49,6 +49,14 @@ while (x !=1){
 3 6 9
 4 8 12 16
 Regra
+n n*2 n*3 .... n*n
+for (int i=1; i<=5; i++){
+    for (int j=1;j<=5;j++){
+ //   if(i%2==0){
+        System.out.println(i*j+"-");
+    }
+    }
+}
 
 
     }
